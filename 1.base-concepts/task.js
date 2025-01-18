8a9ee182a4e40e3a15c9754774fe9b6007220cca
Streltsova <askobina3@gmail.com>
2025-01-18 19:05:14 +0700

    first work

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,18 @@
 "use strict"
-function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
-}
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+function solveEquation(a, b, c) {
+	let arr = [];
+	let d = b ** 2 - 4 * a * c;
+	if (d < 0) {
+		return;
+	} else if (d === 0) {
+		return +(-b / (2 * a));
+	} else {
+		var root1 = (-b + Math.sqrt(d)) / (2 * a);
+		var root2 = (-b - Math.sqrt(d)) / (2 * a);
+		return +root1 + ' и ' + root2;
+	}
+	console.log(solveEquation())
+
+	function calculateTotalMortgage(percent, contribution, amount, countMonths) {}
 }
