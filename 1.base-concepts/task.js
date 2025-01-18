6fb1f4f2e4b6e2c2b2f8e4f41c008ea7bbfa95b9
Streltsova <askobina3@gmail.com>
2025-01-18 21:29:16 +0700

    first project

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,19 +2,19 @@
 
 function solveEquation(a, b, c) {
 	let arr = [];
-  let root1;
-  let root2;  
+	let root1;
+	let root2;
 	let d = b ** 2 - 4 * a * c;
 	if (d < 0) {
-    arr.push[0];
-  } else if (d === 0) {
-		root1 = - b / (2 * a);
-arr.push (root1);
-} else if (d > 0) {
-    let root1 = (- b + Math.sqrt(d)) / (2 * a);
-		let root2 = (- b - Math.sqrt(d)) / (2 * a);
-		arr.push (root1, root2);
+		arr.push[0];
+	} else if (d === 0) {
+		root1 = -b / (2 * a);
+		arr.push(root1);
+	} else if (d > 0) {
+		let root1 = (-b + Math.sqrt(d)) / (2 * a);
+		let root2 = (-b - Math.sqrt(d)) / (2 * a);
+		arr.push(root1, root2);
+	}
+	return arr;
 }
-return arr;
-} 
 
