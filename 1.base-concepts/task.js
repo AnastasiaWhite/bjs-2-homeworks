1f2a49dea621ac55fe85496533664a34bd93b148
Streltsova <askobina3@gmail.com>
2025-01-20 20:57:51 +0700

    first project

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,20 +1,17 @@
 "use strict"
 
-function solveEquation(a, b, c) {
-	let arr = [];
-	let root1;
-	let root2;
-	let d = b ** 2 - 4 * a * c;
-	if (d < 0) {
-		arr.push[0];
-	} else if (d === 0) {
-		root1 = -b / (2 * a);
-		arr.push(root1);
-	} else if (d > 0) {
-		let root1 = (-b + Math.sqrt(d)) / (2 * a);
-		let root2 = (-b - Math.sqrt(d)) / (2 * a);
-		arr.push(root1, root2);
-	}
-	return arr;
-}
 
+function solveEquation(a, b, c) {
+	let d = b ** 2 - 4 * a * c;
+  let arr =[];
+  if (d > 0) {
+    let rootOne = (-b + Math.sqrt(d)) / (2 * a);
+    let rootTwo = (-b - Math.sqrt(d)) / (2 * a);
+    return [rootOne, rootTwo];
+} else if (d === 0) {
+  let rootOne = -b / (2 * a);
+  return [rootOne];
+} else if (d < 0) {
+return arr;
+}
+}
