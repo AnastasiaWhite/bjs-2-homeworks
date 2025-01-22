c4a3fe329dc43b43587ddeb376e22cffda1870f1
Streltsova <askobina3@gmail.com>
2025-01-22 20:44:35 +0700

    first project

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,16 +2,24 @@
 
 
 function solveEquation(a, b, c) {
-	let d = b ** 2 - 4 * a * c;
-  let arr =[];
+  let d = b ** 2 - 4 * a * c;
   if (d > 0) {
-    let rootOne = (-b + Math.sqrt(d)) / (2 * a);
-    let rootTwo = (-b - Math.sqrt(d)) / (2 * a);
-    return [rootOne, rootTwo];
-} else if (d === 0) {
-  let rootOne = -b / (2 * a);
-  return [rootOne];
-} else if (d < 0) {
-return arr;
+    return [(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)];
+  } else if (d === 0) {
+    return [-(b / (2 * a))];
+  } else if (d < 0) {
+    return [];
+  }
 }
+
+function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+  if (percent <= 0 || contribution < 0 || amount <= 0 || countMonths <= 0) {
+
+  }
+  const monthlyRate = (percent / 100) / 12;
+  const loanBody = amount - contribution;
+  const monthlyPayment = loanBody * (monthlyRate + (monthlyRate / (Math.pow(1 + monthlyRate, countMonths) - 1)));
+
+  const totalPayment = (monthlyPayment * countMonths).toFixed(2);
+  return Number(totalPayment);
 }
